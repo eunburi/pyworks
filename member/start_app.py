741bf17b2edbb9b23c391a1b556c08baa2acdc95
eunburi <finestar23@naver.com>
2023-05-04 18:22:58 +0900

    0504 scraping

--- member/start_app.py
+++ member/start_app.py
@@ -1,69 +1,72 @@
 # 컨트롤러 start_app.py
 # templates 폴더, static 폴더
 # 웹 서버 - flask
-from flask import Flask, render_template, request, redirect, url_for, session
+from flask import Flask, render_template, request, \
+    redirect, url_for, session
 import sqlite3
 
 app = Flask(__name__)
 
-app.secret_key = "asdfqwer1234@#$%"
+app.secret_key = "asdfqqewr1234%#@"
 
 # sqlite3와 연동 - 연결 객체 생성
-def getcoon():
+def getconn():
     conn = sqlite3.connect("c:/green_project/sqlworks/pydb/memberdb.db")
     return conn
-print(getcoon())
+print(getconn())
 
 # url - '/' 경로 설정
 @app.route('/', methods = ['GET'])
 def index():
     return render_template('index.html')
 
-# 회원목록
+# 회원 목록
 @app.route('/memberlist', methods = ['GET'])
 def memberlist():
-    conn = getcoon()
+    conn = getconn()
     cursor = conn.cursor()
     sql = "SELECT * FROM member"
     cursor.execute(sql)  # 검색 수행
     rs = cursor.fetchall()
     conn.close()
-
+    # db에 저장된 자료(rs)를 memberlist.html에 보내줌
     return render_template('memberlist.html', rs=rs)
 
-#회원가입
-@app.route('/register', methods = ['GET','POST'])
+# 회원 가입
+@app.route('/register', methods = ['GET', 'POST'])
 def register():
     if request.method == 'POST':
-        #가입 폼에 입력한 자료 넘겨받음
+        # 가입 폼에 입력된 자료 넘겨 받음
         id = request.form['memberid']
         pw = request.form['passwd1']
         name = request.form['name']
         gender = request.form['gender']
 
         #db에 저장
-        conn = getcoon()
+        conn = getconn()
         cursor = conn.cursor()
         sql = f"INSERT INTO member(memberid, passwd, name, gender) " \
-              f"VALUES('{id}','{pw}','{name}','{gender}')"
+              f"VALUES ('{id}', '{pw}', '{name}', '{gender}')"
+        # 자동 로그인 - 세션 발급
+        session['userid'] = request.form['memberid']
         cursor.execute(sql)  # 검색 수행
-        conn.commit()  # 커밋완료
+        conn.commit()  # 커밋 완료
         conn.close()
-        # redirect - 회원가입 후 회원목록페이지로 이동 시킴
+        # redirect - 회원가입후 회원 목록 페이지로 강제로 이동 시킴
         return redirect(url_for('memberlist'))
     else:
-       return render_template('register.html')
+        return render_template('register.html')
 
 #로그인 페이지
-@app.route('/login', methods = ['GET','POST'])
+@app.route('/login', methods = ['GET', 'POST'])
 def login():
     if request.method == "POST":
-        #로그인폼에 입력된 데이터 넘겨 받음
+        # 로그인폼에 입력된 데이터 넘겨 받음
         id = request.form['memberid']
         pw = request.form['passwd']
 
         #database에 접속 - 로그인 체크
-        conn = getcoon()
+        conn = getconn()
         cursor = conn.cursor()
         # 동적 바인딩
         sql = f"SELECT * FROM member " \
@@ -73,8 +76,8 @@ def login():
         print(rs)
         conn.close()
 
-        if rs: #rs = true
-            session['userid'] = rs[0]   #memberid를 세션에 저장(세견발급)
+        if rs: # rs = True
+            session['userid'] = rs[0]  #memberid를 세션에 저장(세션 발급)
             return redirect(url_for('index'))
         else:
             error_msg = "아이디나 비밀번호를 확인해주세요"
@@ -84,7 +87,7 @@ def login():
 
 @app.route('/logout')
 def logout():
-    session.clear() #모든 세션 삭제
+    session.clear()  #모든 세션 삭제
     return redirect(url_for('index'))
 
 app.run()
--- pyui/control-tool.py
+++ pyui/control-tool.py
@@ -1,4 +1,4 @@
-#컨트롤 도구 - 클래스 사용 생성
+# 컨트롤 도구 - 클래스 사용 생성
 
 from tkinter import *
 
@@ -7,12 +7,12 @@ def click():
 
 root = Tk()
 root.title("UI 구성")
-root.geometry("200x50")
+root.geometry("200x100")
 frame = Frame(root)
-frame.pack()
+frame.pack()  # 가운데 정렬
 
 Label(frame, text="").grid(row=0, column=0)
-Button(frame, text='제출', command=click).grid(row=0, column=0)
+Button(frame, text='제출', command=click).grid(row=1, column=0)
 lbl = Label(frame, text="안녕하세요")
 lbl.grid(row=2, column=0)
 
