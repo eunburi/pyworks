9ac297e7735099173b9010032dd6d25e731af580
eunburi <finestar23@naver.com>
2023-05-11 18:16:51 +0900

    'hower

--- tiny/picnic.py
+++ tiny/picnic.py
@@ -5,11 +5,12 @@ def get_args():
         formatter_class=argparse.ArgumentDefaultsHelpFormatter
     )
 
-    # 명령행 인수 : -n(축약형), --name
+    # 아이템을 여러개 전달(사용)
     parser.add_argument('item',
                         metavar='str',
                         nargs='+',  #인수를 1개 이상 전달함
                         help='Item(s) to bring')
+    # -s, --sorted를 사용하면 정렬 기능을 사용
     parser.add_argument('-s',
                        '--sorted',
                        action='store_true',
